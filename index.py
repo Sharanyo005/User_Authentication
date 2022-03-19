bf77884dd9091099388c300e8053f742869b751f
Maharaj <89255792+Sharanyo005@users.noreply.github.com>
2022-03-19 13:18:00 +0530

    added file path

--- index.py
+++ index.py
@@ -1,27 +1,20 @@
 from time import sleep as s
- 
-
 def signUp(username, password):
-    with open('Your_File_path', 'r') as f:
+    with open('database.txt', 'r') as f:
         txt = f.readlines()   
     if f"{username},{password}\n" in txt:
         print("Processing...")    
         s(1)
         print("You have already Signed Up...try to Log in")
-    
     if f"{username},{password}\n" not in txt:
         print("Processing...")
         s(1)
-        with open("Your_File_path", 'a') as f:
+        with open("database.txt", 'a') as f:
             f.write(f'{username},{password}\n')
         print("You have Successfully Signed Up!")
-
-
-
 def logIn(username, password):
-    with open('Your_File_path', 'r') as f:
+    with open('database.txt', 'r') as f:
         txt = f.readlines()
-
     if f"{username},{password}\n" in txt:
         print("Correct!") 
         id = txt.index(f"{username},{password}\n")
@@ -33,15 +26,11 @@ def logIn(username, password):
         print("Processing...")
         s(1)
         print("Incorrect Username or Password...(May be you haven't signed up yet)")
-
-
-
 while True:
     choice = input('''
 If You are new here, Sign Up type 'S' to Sign Up, Or If You Have Already Signed Up, type 'L' to Log in...!
 
 Sign up / Log in [S/L]: ''')
-
     if choice.upper() == 'S':
         print('''
                 SIGN UP      
@@ -51,7 +40,6 @@ Sign up / Log in [S/L]: ''')
         pswd = input("Password: ")
         signUp(uname, pswd)
         break
-        
     elif choice.upper() == 'L': 
         print('''
                 LOG IN      
